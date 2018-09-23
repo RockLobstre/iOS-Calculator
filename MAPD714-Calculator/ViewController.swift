35bdd1cfb90428632011c3af7dee4899ce6d0429
matin <hsalehi91@gmail.com>
2018-09-23 16:35:39 -0400

    + storyboard changed with new fields such as result field + new outlet and actions added + basic action functionalities for buttons added 	- number 	- operation 	- equals 	- clear

--- MAPD714-Calculator/ViewController.swift
+++ MAPD714-Calculator/ViewController.swift
@@ -11,28 +11,100 @@ import Pastel
 import RevealingSplashView
 
 class ViewController: UIViewController {
-
+    // OUTLETS
+    @IBOutlet weak var resultField: UILabel!
+    @IBOutlet weak var equationFiels: UILabel!
+    
+    //VARIABLES
+    var firstOperand = ""
+    var secondOperand = ""
+    var op = ""
+    var isOperation = false
+    
+    
+/*********************************************************************************************************/
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        //Initialize Background Floating Gradient (Pastel)
         initializePastel()
         
-        //Initialize a revealing Splash with with the iconImage, the initial size and the background color
+        //Initialize a revealing Splash
         let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "appLogo")!,iconInitialSize: CGSize(width: 170, height: 170), backgroundColor: UIColor(red:156/255, green:39/255, blue:176/255, alpha:1.0))
         
         //Adds the revealing splash view as a sub view
         self.view.addSubview(revealingSplashView)
         revealingSplashView.animationType = SplashAnimationType.popAndZoomOut
-        
-        //Starts animation
+            //Starts animation
         revealingSplashView.startAnimation(){}
     }
     
+    //handle orientaion changes
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         initializePastel()
     }
-
+    
+/*  CASE1: NUMBER PRESSED ************************************************************
+    */
+    @IBAction func numbersPressed(_ sender: UIButton) {
+        let number = sender.titleLabel?.text
+        if(!isOperation){
+            firstOperand = firstOperand + number!
+        } else {
+            secondOperand = secondOperand + number!
+        }
+        resultField.text = resultField.text! + number!
+    }
+    
+/*  CASE2: OPERATION PRESSED ************************************************************
+     */
+    @IBAction func operationPressed(_ sender: UIButton) {
+        let operation = sender.titleLabel?.text
+        switch operation {
+        case "-","+","x","/","%":
+            op = operation!
+            isOperation = true
+        default:
+            resultField.text = "0"
+        }
+    }
+    
+/*  CASE3: EQUALS BUTTON PRESSED ************************************************************
+     */
+    @IBAction func calculatePress(_ sender: UIButton) {
+        switch op {
+        case "+":
+            resultField.text = String(Double(firstOperand)! + Double(firstOperand)!)
+            firstOperand = resultField.text!
+            secondOperand = ""
+        case "-":
+            resultField.text = String(Double(firstOperand)! - Double(firstOperand)!)
+            firstOperand = resultField.text!
+            secondOperand = ""
+        case "x":
+            resultField.text = String(Double(firstOperand)! * Double(firstOperand)!)
+            firstOperand = resultField.text!
+            secondOperand = ""
+        case "/":
+            resultField.text = String(Double(firstOperand)! / Double(firstOperand)!)
+            firstOperand = resultField.text!
+            secondOperand = ""
+        default:
+            resultField.text = "0"
+        }
+    }
+    
+/*  CASE4: CLEAR PRESSED ************************************************************
+     */
+    @IBAction func clearPressed(_ sender: UIButton) {
+        firstOperand = ""
+        secondOperand = ""
+        op = ""
+        isOperation = false
+        resultField.text = "0"
+    }
+    
+/*********************************************************************************************/
     func initializePastel() {
         let pastelView = PastelView(frame: view.bounds)
         
